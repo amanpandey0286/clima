7463f9094c3bd1d357b7b7918c5b86c2ba336256
amanpandey0286 <amanpandey.2020@vitbhopal.ac.in>
2022-02-01 01:41:53 +0530

    first commit

---
+++ lib/services/location.dart
@@ -0,0 +1,21 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double latitude;
+  double longitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      LocationPermission permission;
+      permission = await Geolocator.requestPermission();
+
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      latitude = position.latitude;
+      longitude = position.longitude;
+      print(position);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
